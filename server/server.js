fe647c450105a4186638ffecd57d027cae773854
Sia Hua Jiuh <ahjiuhdotcom@gmail.com>
2017-05-09 04:24:05 +0000

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -18,17 +19,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
-    socket.emit('newMessage', {
-       from: 'Admin',
-       text: 'Welcome to the chat app',
-       createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
     /*
     socket.emit('newMessage', {
@@ -44,11 +37,7 @@ io.on('connection', (socket) => {
         // 'io.emit' emit an event to every single connection
         // overall mechanism: when receive a message from certain client,
         // emit it to all the rest of the client who maintain the connection
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         
         /*
         // ''.broadcast': emit an event to all connected client 
---
+++ server/utils/message.test.js
@@ -0,0 +1,17 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        const from = 'Jen';
+        const text = 'Some message';
+        const message = generateMessage(from, text);
+        
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            text
+        });
+    });
+});
